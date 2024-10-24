f421f05b7432d4de0cbc4d595202df909ca3850b
Jean Ribeiro <contact@jeanribeiro.dev>
2024-10-23 21:57:34 -0300

    refactor: acc_options.php

--- src/acc_options.php
+++ src/acc_options.php
@@ -1,18 +1,44 @@
 <?php
-	include("lib.php");
-	define("PAGENAME", "Opções da conta");
-	$acc = check_acc($secret_key, $db);
+include("lib.php");
+define("PAGENAME", "Opções da conta");
+$acc = check_acc($secret_key, $db);
 
-	include("templates/acc-header.php");
-    echo "<span id=\"aviso-a\"></span>";
-    
-	echo "<br/><p>";
-	echo "<center><a href=\"accpass.php\">Alterar senha desta conta.</a><br/></center>";
-	echo "<center><a href=\"changemail.php\">Alterar email desta conta.</a><br/></center>";
-	echo "<center><a href=\"editinfo.php\">Alterar configurações pessoais.</a><br/></center>";
-    	echo "<center><a href=\"transferchar.php\">Transferir personagem para esta conta.</a><br/><br/></center>";
+include("templates/acc-header.php");
 
-	echo "<center><font size=\"1px\"><a href=\"characters.php\"><b>Voltar</b></a> - <a href=\"#\" onclick=\"javascript:window.open('accountlog.php', '_blank','top=100, left=100, height=350, width=520, status=no, menubar=no, resizable=no, scrollbars=yes, toolbar=no, location=no, directories=no');\">Exibir logs da conta</a></font></center><br/></p>";
+function generateAccountOptionLink($url, $text)
+{
+	return "<center><a href=\"$url\">$text</a><br/></center>";
+}
+?>
 
-	include("templates/acc-footer.php");
+<span id="aviso-a"></span>
+
+<br />
+<p>
+	<?php
+	$accountOptions = [
+		'accpass.php' => 'Alterar senha desta conta.',
+		'changemail.php' => 'Alterar email desta conta.',
+		'editinfo.php' => 'Alterar configurações pessoais.',
+		'transferchar.php' => 'Transferir personagem para esta conta.'
+	];
+
+	foreach ($accountOptions as $url => $text) {
+		echo generateAccountOptionLink($url, $text);
+	}
+	?>
+
+	<br />
+	<center>
+		<font size="1px">
+			<a href="characters.php"><b>Voltar</b></a> -
+			<a href="#" onclick="javascript:window.open('accountlog.php', '_blank','top=100, left=100, height=350, width=520, status=no, menubar=no, resizable=no, scrollbars=yes, toolbar=no, location=no, directories=no');">
+				Exibir logs da conta
+			</a>
+		</font>
+	</center>
+</p>
+
+<?php
+include("templates/acc-footer.php");
 ?>
