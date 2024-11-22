438bad0ce79049cbece87fabb2cd862e34f9496e
Kagijin <Clintoncorreia1995@gmail.com>
2024-11-22 09:23:53 -0300

    fix monster 2

--- src/monster.php
+++ src/monster.php
@@ -461,11 +461,11 @@ switch ($_GET['act']) {
 		$forcadoplayer = ceil((($player->strength + ($player->atkbonus['effectiveness'] ?? 0) + (($player->atkbonus['item_bonus'] ?? 0) * 2) + $pbonusfor) * $multipleatk) * 1.5);
 		$agilidadedoplayer = ceil(
 			$player->agility + 
-			($player->agibonus6['effectiveness'] ?? 0) + 
-			($player->agibonus7['effectiveness'] ?? 0) + 
+			(($player->agibonus6['effectiveness'] ?? 0) + 
+			($player->agibonus7['effectiveness'] ?? 0)) + 
 			(($player->agibonus6['item_bonus'] ?? 0) * 2) + 
 			(($player->agibonus7['item_bonus'] ?? 0) * 2) + 
-			$pbonusagi
+			($pbonusagi ?? 0)
 		);		
 		$resistenciadoplayer = ceil((($player->resistance + (($player->defbonus1['effectiveness'] ?? 0) + ($player->defbonus2['effectiveness'] ?? 0) + ($player->defbonus3['effectiveness'] ?? 0) + ($player->defbonus5['effectiveness'] ?? 0)) + ((($player->defbonus1['item_bonus'] ?? 0) * 2) + (($player->defbonus2['item_bonus'] ?? 0) * 2) + (($player->defbonus3['item_bonus'] ?? 0) * 2) + (($player->defbonus5['item_bonus'] ?? 0) * 2)) + $pbonusres) * $multipledef) / 0.85);
 
@@ -720,11 +720,6 @@ switch ($_GET['act']) {
 				if ($setting->eventoouro > time()) {
 					$goldwin = round($goldwin * 4);
 				}
-<<<<<<< Updated upstream
-        
-=======
-
->>>>>>> Stashed changes
 				// Here, the gold gain rate after a kill is adjusted
 				$goldwin = round($goldwin * $rate_gold);
 				$goldwin *= $bixo->mul;
